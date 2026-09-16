395372df7ce1954c2d1a3a9cdc971dfefe6b846b
KarthikStackly <mallareddisaikarthik@thestackly.com>
2026-09-16 14:47:49 +0530

    AnotherSpotExercise

---
+++ 3_Java/AnotherSpotExercise/src/AddTo100.java
@@ -0,0 +1,9 @@
+public class AddTo100 {
+    static void main() {
+        int total = 0;
+        for (int i = 1; i <= 100; i++){
+            total += i;
+        }
+        System.out.println("Total is " + total);
+    }
+}
---
+++ 3_Java/AnotherSpotExercise/src/CountDown10.java
@@ -0,0 +1,8 @@
+public class CountDown10 {
+    static void main() {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("BOOM!!!!");
+    }
+}
